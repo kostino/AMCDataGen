094a673e8147da9192d82d34b2eb880570939888
CedArctic <kdoumani98@gmail.com>
2021-04-30 20:55:31 +0300

    Moved IQ->XY plane conversion logic to seperate function

--- imgConstellation/imgGen.py
+++ imgConstellation/imgGen.py
@@ -2,6 +2,36 @@ import numpy as np
 from PIL import Image
 
 
+# === Function to map from I/Q plane to X-Y image plane ===
+def IQtoXY(symbols, i_range, q_range, img_resolution):
+    '''
+    Maps complex I/Q samples to an X-Y plane compatible with 2D array indices
+
+    :param symbols: Complex I/Q samples array
+    :param i_range: Tuple indicating the range of I to be converted e.g: (-7,7)
+    :param q_range: Tuple indicating the range of Q to be converted e.g: (-7,7)
+    :param img_resolution: Target X-Y plane dimensions e.g: (200,200) creates a 200x200 image
+    :return: x_samples, y_samples: X and Y coordinates of converted samples
+    '''
+    # Samples to be transformed
+    i_samples = symbols.real
+    q_samples = symbols.imag
+    # Transform samples from continuous I/Q plane to continuous image plane
+    y_samples = (i_samples + np.abs(i_range[0])) * img_resolution[1] / (i_range[1] - i_range[0])
+    x_samples = - (q_samples - q_range[1]) * img_resolution[0] / (q_range[1] - q_range[0])
+    # Clip samples outside the pixel range
+    # Find samples going over the maximum x value and remove them
+    x_mask = x_samples >= img_resolution[0]
+    x_samples = np.delete(x_samples, x_mask)
+    y_samples = np.delete(y_samples, x_mask)
+    # Find samples going over the maximum y value and remove them
+    y_mask = y_samples >= img_resolution[1]
+    x_samples = np.delete(x_samples, y_mask)
+    y_samples = np.delete(y_samples, y_mask)
+    # Return
+    return x_samples, y_samples
+
+
 # === Grayscale Image Generation - Section III-B ===
 def grayscale(symbols, i_range, q_range, img_resolution, filename):
     '''
@@ -14,29 +44,12 @@ def grayscale(symbols, i_range, q_range, img_resolution, filename):
     :param filename: Output image file name
     :return:
     '''
-    # Image resolution (x, y)
-    # img_resolution = (200, 200)
-    # I/Q area to map to image
-    # i_range = (-7, 7)
-    # q_range = (-7, 7)
-    # Samples to be transformed
-    i_samples = symbols.real
-    q_samples = symbols.imag
-    # Transform samples from continuous I/Q plane to continuous image plane
-    y_samples = (i_samples + np.abs(i_range[0])) * img_resolution[1] / (i_range[1] - i_range[0])
-    x_samples = - (q_samples - q_range[1]) * img_resolution[0] / (q_range[1] - q_range[0])
+
+    # Transform I/Q samples to XY plane
+    x_samples, y_samples = IQtoXY(symbols, i_range, q_range, img_resolution)
     # Quantize samples to pixel values using floor
     y_samples = np.floor(y_samples)
     x_samples = np.floor(x_samples)
-    # Clip samples outside the pixel range
-    # Find samples going over the maximum x value and remove them
-    x_mask = x_samples >= img_resolution[0]
-    x_samples = np.delete(x_samples, x_mask)
-    y_samples = np.delete(y_samples, x_mask)
-    # Find samples going over the maximum y value and remove them
-    y_mask = y_samples >= img_resolution[1]
-    x_samples = np.delete(x_samples, y_mask)
-    y_samples = np.delete(y_samples, y_mask)
     # Cast to integers
     x_samples = x_samples.astype(int)
     y_samples = y_samples.astype(int)
@@ -75,22 +88,9 @@ def enhancedGrayscale(symbols, i_range, q_range, img_resolution, filename, power
     :param decay: Exponential decay coefficient
     :return:
     '''
-    # Samples to be transformed
-    i_samples = symbols.real
-    q_samples = symbols.imag
-    # Transform samples from continuous I/Q plane to continuous image plane
-    y_samples = (i_samples + np.abs(i_range[0])) * img_resolution[1] / (i_range[1] - i_range[0])
-    x_samples = - (q_samples - q_range[1]) * img_resolution[0] / (q_range[1] - q_range[0])
+    # Transform I/Q samples to XY plane
+    x_samples, y_samples = IQtoXY(symbols, i_range, q_range, img_resolution)
 
-    # Clip samples outside the pixel range
-    # Find samples going over the maximum x value and remove them
-    x_mask = x_samples >= img_resolution[0]
-    x_samples = np.delete(x_samples, x_mask)
-    y_samples = np.delete(y_samples, x_mask)
-    # Find samples going over the maximum y value and remove them
-    y_mask = y_samples >= img_resolution[1]
-    x_samples = np.delete(x_samples, y_mask)
-    y_samples = np.delete(y_samples, y_mask)
     # Number of final samples
     samples_num = len(x_samples)
 
