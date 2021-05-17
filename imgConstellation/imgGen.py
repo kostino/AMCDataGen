Fix global norm usage in channel normalization

imgConstellation/imgGen.py
@@ -80,7 +80,7 @@ def grayscaleImgGen(symbols, i_range, q_range, img_resolution, filename):
 
 
 # === Enhanced Grayscale and RGB Image Generation - Section III-C&D ===
-def enhancedImgGen(symbols, i_range, q_range, img_resolution, filename, channels, power, decay, global_norm):
+def enhancedImgGen(symbols, i_range, q_range, img_resolution, filename, channels, power, decay, global_norm=False):
     """
     Generates Enhanced Grayscale and RGB Images from complex I/Q samples using exponential decay.
 
@@ -92,7 +92,7 @@ def enhancedImgGen(symbols, i_range, q_range, img_resolution, filename, channels
     :param channels: Number of image channels: 1 -> Grayscale, 3 -> RGB
     :param power: Tuple for power of I/Q samples on each layer/channel
     :param decay: Tuple for exponential decay coefficient for each layer/channel
-    :param globalNorm: Whether to normalize the image pixels on a global-across all channels or on a per-channel basis
+    :param global_norm: Whether to normalize the image pixels on a global-across all channels or on a per-channel basis
     :return:
     """
     # Transform I/Q samples to XY plane
@@ -130,7 +130,7 @@ def enhancedImgGen(symbols, i_range, q_range, img_resolution, filename, channels
 
     # Prepare for grayscale image
     # Normalize Grid Array to 255 (8-bit pixel value)
-    if global_norm:
+    if not global_norm:
         # Normalize on a global basis
         normalized_grid = (power_grid / np.max(power_grid, axis=(0, 1)).reshape((1, 1, channels))) * 255
     else:
@@ -155,7 +155,7 @@ def enhancedImgGen(symbols, i_range, q_range, img_resolution, filename, channels
 
 
 # === Enhanced Grayscale and RGB Image Generation - Section III-C&D ===
-def enhancedImgGenCUDA(symbols, i_range, q_range, img_resolution, filename, channels, power, decay):
+def enhancedImgGenCUDA(symbols, i_range, q_range, img_resolution, filename, channels, power, decay, global_norm=False):
     """
     Generates Enhanced Grayscale and RGB Images from complex I/Q samples using exponential decay.
 
@@ -167,6 +167,7 @@ def enhancedImgGenCUDA(symbols, i_range, q_range, img_resolution, filename, chan
     :param channels: Number of image channels: 1 -> Grayscale, 3 -> RGB
     :param power: Tuple for power of I/Q samples on each layer/channel
     :param decay: Tuple for exponential decay coefficient for each layer/channel
+    :param global_norm: Whether to normalize the image pixels on a global-across all channels or on a per-channel basis
     :return:
     """
     # Transform I/Q samples to XY plane
@@ -203,7 +204,12 @@ def enhancedImgGenCUDA(symbols, i_range, q_range, img_resolution, filename, chan
     power_grid = cp.asnumpy(cp.sum(powergrid, axis=4))
 
     # Normalize Grid Array to 255 (8-bit pixel value)
-    normalized_grid = (power_grid / np.max(power_grid, axis=(0, 1)).reshape((1, 1, channels))) * 255
+    if not global_norm:
+        # Normalize on a global basis
+        normalized_grid = (power_grid / np.max(power_grid, axis=(0, 1)).reshape((1, 1, channels))) * 255
+    else:
+        # Normalize on a per channel basis
+        normalized_grid = (power_grid / np.max(power_grid)) * 255
     # Quantize grid to integers
     normalized_grid = np.floor(normalized_grid)
     # Copy result to uint8 array for writing grayscale image