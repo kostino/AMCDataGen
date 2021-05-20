09d1b5fb517a435c11f03eb0754b1e755addd683
unknown <triaridis@ece.auth.gr>
2021-05-21 01:19:00 +0300

    Demo and benchmarks for cuda vs numexpr for image generation

---
+++ imgConstellation/democuda.py
@@ -0,0 +1,35 @@
+from constellations import *
+import time
+import numpy as np
+import cupy as cp
+import numexpr as ne
+from cupyx import optimizing
+
+fourPAM = PAM("4-PAM", 4, 1, 0)
+image_ns = [1, 2, 3, 5, 10, 12]
+numexpr_times = []
+cupy_times = []
+for image_n in image_ns:
+    print("IMAGES: {}".format(image_n))
+    samplesCPU = []
+    for i in range(image_n):
+        samplesCPU.append(fourPAM.sampleGenerator(1000, SNR=10))
+    samples = fourPAM.sampleGenerator(1000*image_n, SNR=10)
+
+
+    start_time = time.time()
+    for i in range(image_n):
+        samplesCPU[i].enhancedRGB((224, 224), "TIMERGB_{}.png".format(i), bounds=((-3.5, 3.5), (-3.5, 3.5)))
+    numexpr_times.append(time.time() - start_time)
+    #print("NUMEXPR time: {}".format(time.time() - start_time))
+
+
+    start_time = time.time()
+    samples.enhancedRGBCUDABATCH((224, 224), "TIMECUDA.png", n_images=image_n, bounds=((-3.5, 3.5), (-3.5, 3.5)))
+    cupy_times.append(time.time() - start_time)
+    #print("CUPY time: {}".format(time.time() - start_time))
+
+speedup = np.array(numexpr_times) / np.array(cupy_times)
+print(speedup)
+plt.plot(image_ns, speedup)
+plt.show()
