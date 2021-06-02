6ae29ce25563336d37a8a914a5d6db3395d2309a
CedArctic <kdoumani98@gmail.com>
2021-06-02 19:49:21 +0300

    Added cumulants and moments calculations for signal. Amplitude and Phase will be completed in following commits.

--- imgConstellation/samples.py
+++ imgConstellation/samples.py
@@ -32,6 +32,14 @@ class Samples:
         self.rng = np.random.default_rng()
         # Save average symbol power
         self.symbol_power = symbol_power
+        # Declare moments array - run calculateMoments to fill
+        self.amp_moments = None
+        self.phase_moments = None
+        self.signal_moments = None
+        # Declare moments array - run calculateMoments to fill
+        self.amp_cumulants = None
+        self.phase_cumulants = None
+        self.signal_cumulants = None
 
         # Generate random indices and use them to generate a series of symbols samples
         indexes = self.rng.integers(0, len(symbols), samples_num)
@@ -48,6 +56,59 @@ class Samples:
         scale = offset + self.max_range
         self.irange = (-scale, scale)
         self.qrange = (-scale, scale)
+
+    def calculateMoment(self, p, q):
+        """
+        Return moment M_p,q
+        :param p: Parameter p
+        :param q: Parameter q
+        :return: M_pq
+        """
+        conj = np.conjugate(self.samples)
+        return np.mean(np.power(self.samples, p-q) * np.power(conj, q))
+
+    def calculateMoments(self):
+        # # Amplitude & Phase
+        # amp = np.abs(self.samples)
+        # phase = np.angle(self.samples)
+        #
+        # self.amp_moments = {"M{}0".format(i): stats.moment(amp, i) for i in range(start=1, stop=7, step=1)}
+        # self.phase_moments = {"M{}0".format(i): stats.moment(phase, i) for i in range(start=1, stop=7, step=1)}
+        self.signal_moments = {"M{}{}".format(p,q): self.calculateMoment(p, q) for p in range(2, 7)
+                               for q in range(0, 4) if q <= p}
+        return self
+
+    def calculateCumulants(self):
+        # Call moments calculation
+        self.calculateMoments()
+
+        # Calculate Signal Cumulants
+        self.signal_cumulants = {}
+        self.signal_cumulants["C20"] = self.signal_moments["M20"]
+        self.signal_cumulants["C21"] = self.signal_moments["M21"]
+        self.signal_cumulants["C40"] = self.signal_moments["M40"] - 3 * np.power(self.signal_moments["M20"], 2)
+        self.signal_cumulants["C41"] = self.signal_moments["M41"] \
+                                       - 3 * self.signal_moments["M20"] * self.signal_moments["M21"]
+        self.signal_cumulants["C42"] = self.signal_moments["M42"] - np.power(np.abs(self.signal_moments["M20"]), 2) \
+                                       - 2 * np.power(self.signal_moments["M21"], 2)
+        self.signal_cumulants["C60"] = self.signal_moments["M60"] - 15 * self.signal_moments["M20"] * self.signal_moments["M40"] \
+                                       + 3 * np.power(self.signal_moments["M20"], 3)
+        self.signal_cumulants["C61"] = self.signal_moments["M61"] - 5 * self.signal_moments["M21"] * self.signal_moments["M40"] \
+                                       - 10 * self.signal_moments["M20"] * self.signal_moments["M41"] \
+                                       + 30 * np.power(self.signal_moments["M20"], 2) * self.signal_moments["M21"]
+        self.signal_cumulants["C62"] = self.signal_moments["M62"] - 6 * self.signal_moments["M20"] * self.signal_moments["M42"] \
+                                       - 8 * self.signal_moments["M21"] * self.signal_moments["M41"] \
+                                       - self.signal_moments["M22"] * self.signal_moments["M40"] \
+                                       + 6 * np.power(self.signal_moments["M20"], 2) * self.signal_moments["M22"] \
+                                       + 24 * np.power(self.signal_moments["M21"], 2) * self.signal_moments["M20"]
+        self.signal_cumulants["C63"] = self.signal_moments["M63"] - 9 * self.signal_moments["M21"] * self.signal_moments["M42"] \
+                                       + 12 * np.power(self.signal_moments["M21"], 3) \
+                                       - 3 * self.signal_moments["M20"] * self.signal_moments["M43"] \
+                                       - 3 * self.signal_moments["M22"] * self.signal_moments["M41"]  \
+                                       + 18 * self.signal_moments["M20"] * self.signal_moments["M21"] * self.signal_moments["M22"]
+
+        return self
+
 
     """ Channel Impairments functions """
     def awgn(self, SNR):
